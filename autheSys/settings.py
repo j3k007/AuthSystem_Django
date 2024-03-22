e550876a12490eeed752a747ee95edde7ad05dc4
Jatin Koli <70892665+j3k007@users.noreply.github.com>
2024-03-22 23:08:19 +0530

    crispy forms is added

--- autheSys/settings.py
+++ autheSys/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
+    'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
