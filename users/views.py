a716ebf8d844037fac3a7070bb07185190259217
Jatin Koli <70892665+j3k007@users.noreply.github.com>
2024-03-24 17:06:55 +0530

    Added view function for login and JWT token generation

--- users/views.py
+++ users/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView, Response
+from rest_framework.exceptions import AuthenticationFailed
 from . serializers import UserSerializer
+from . models import User
+import logging
+
 
 # Create your views here.
+
+logger = logging.getLogger("__name__")
 
 class Register(APIView):
     def post(self, request):
@@ -10,3 +16,35 @@ class Register(APIView):
         seralizer.is_valid(raise_exception=True)
         seralizer.save()
         return Response(seralizer.data)
+    
+class Login(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            logger.exception("User Not Found !")
+            raise AuthenticationFailed('User Not Found !')
+
+        if user.check_password(password):
+            logger.exception("Password didn't matched !")
+            raise AuthenticationFailed("Password didn't matched !")
+        
+        # payload = {
+        #     'id': user.id,
+        #     'exp': datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=60),
+        #     'iat': datetime.datetime.now(datetime.timezone.utc)
+        # }
+
+        # token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
+        # respose = Response()
+        # respose.set_cookie(key='jwt', value=token, httponly=True)
+        # respose.data = {
+        #     'jwt':token
+        # }
+
+        return Response({
+            "message": "Login Successfully"
+        })
